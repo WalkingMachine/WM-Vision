4407fa2f140d83348a67f8c137fff2c9e472f71e
Keaven Martin <keaven.martin@gmail.com>
2013-05-15 09:52:39 -0400

    VISION-32 Added offset in ParallelLinesNode

--- src/vision_kernel/vision_nodes/parallel_lines_node.cc
+++ src/vision_kernel/vision_nodes/parallel_lines_node.cc
@@ -16,21 +16,31 @@
 #include "../../../include/vision_kernel/vision_nodes/parallel_lines_node.h"
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/gpu/gpu.hpp>
 #include <boost/lexical_cast.hpp>
-#include <ros/ros.h>
 
 enum {X1, Y1, X2, Y2};
 
 Data ParallelLinesNode::Function(InputData input_data) {
-  std::shared_ptr<cv::vector<cv::Vec4i>> lines = input_data->at("input")->DataWithValidation<cv::vector<cv::Vec4i>>();
+  std::shared_ptr<cv::vector<cv::Vec4i>> lines =
+      input_data->at("lines")->DataWithValidation<cv::vector<cv::Vec4i>>();
+
+  std::shared_ptr<int> offset_angle;
+
+  try {
+    offset_angle = input_data->at("offsetAngle")->DataWithValidation<int>();
+  } catch (std::exception e) {
+    offset_angle = 0;
+  }
+
   Data output_data;
 
-  double angle = boost::lexical_cast<double>(parameters()["Angle"]) / 180 * CV_PI;
+  double angle =
+      (boost::lexical_cast<double>(parameters()["Angle"]) + *offset_angle) / 180 * CV_PI;
   if(angle > CV_PI) angle -= CV_PI;
 
-  double tolerance = boost::lexical_cast<double>(parameters()["AngleTolerance"]) / 180 * CV_PI;
+  double tolerance =
+      boost::lexical_cast<double>(parameters()["AngleTolerance"]) / 180 * CV_PI;
 
   // Positif tolerance
   bool positif_tolerance_pass_pi = false;
@@ -57,21 +67,26 @@ Data ParallelLinesNode::Function(InputData input_data) {
 	  bool positif_tolerance_ok, negatif_tolerance_ok;
 
 	  for (auto &line: *lines) {  // Each lines
-		  line_angle = cv::fastAtan2(line[X1] - line[X2], line[Y1] - line[Y2]) / 180 * CV_PI;  // Get line angle
+		  line_angle =
+		      cv::fastAtan2(line[X1] - line[X2], line[Y1] - line[Y2]) / 180 * CV_PI;  // Get line angle
 		  if(line_angle >= CV_PI) line_angle -= CV_PI;
 
       // Positif tolerance validation
       if (positif_tolerance_pass_pi) {
-        positif_tolerance_ok = (line_angle <= positif_tolerance) || (line_angle >= angle);
+        positif_tolerance_ok =
+            (line_angle <= positif_tolerance) || (line_angle >= angle);
       } else {
-        positif_tolerance_ok = (line_angle <= positif_tolerance) && (line_angle >= angle);
+        positif_tolerance_ok =
+            (line_angle <= positif_tolerance) && (line_angle >= angle);
       }
 
       // Negatif tolerance validation
       if (negatif_tolerance_pass_pi) {
-        negatif_tolerance_ok = (line_angle >= negatif_tolerance) || (line_angle <= angle);
+        negatif_tolerance_ok =
+            (line_angle >= negatif_tolerance) || (line_angle <= angle);
       } else {
-        negatif_tolerance_ok = (line_angle >= negatif_tolerance) && (line_angle <= angle);
+        negatif_tolerance_ok =
+            (line_angle >= negatif_tolerance) && (line_angle <= angle);
       }
 
 		  if (negatif_tolerance_ok || positif_tolerance_ok)
