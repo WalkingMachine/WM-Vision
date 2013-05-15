d04291a8ca0a40ea2a84555fa458a842750b90ae
Keaven Martin <keaven.martin@gmail.com>
2013-05-14 23:27:44 -0400

    VISION-32 Better FiltredVerticalLinePairsNode implementation

--- src/vision_kernel/vision_nodes/filtred_vertical_line_pairs_node.cc
+++ src/vision_kernel/vision_nodes/filtred_vertical_line_pairs_node.cc
@@ -23,41 +23,82 @@
 enum {X1, Y1, X2, Y2};
 
 Data FiltredVerticalLinePairsNode::Function(InputData input_data) {
-  std::shared_ptr<cv::vector<cv::Vec4i>> lines = input_data->at("input")->DataWithValidation<cv::vector<cv::Vec4i>>();
-  Data output_data;
+  std::shared_ptr<cv::vector<cv::Vec4i>> lines =
+      input_data->at("input")->DataWithValidation<cv::vector<cv::Vec4i>>();
 
-  std::shared_ptr<std::deque<std::pair<cv::Vec4i, cv::Vec4i>>> filtred_line_pairs(new std::deque<std::pair<cv::Vec4i, cv::Vec4i>>);
+  Data output_data;
+  std::shared_ptr<std::deque<std::pair<cv::Vec4i, cv::Vec4i>>>
+      filtred_line_pairs(new std::deque<std::pair<cv::Vec4i, cv::Vec4i>>);
 
   if (parameters()["IsOnCUDA"] == "true") {
 
   } else {
 	  int *lowest_X_first_line_pointer, *bigest_X_first_line_pointer,
-	      *lowest_X_second_line_pointer, *bigest_X_second_line_pointer;
+	      *lowest_X_second_line_pointer, *bigest_X_second_line_pointer,
+        *lowest_Y_first_line_pointer, *bigest_Y_first_line_pointer,
+        *lowest_Y_second_line_pointer, *bigest_Y_second_line_pointer;
 
 	  // Improved iteration between pairs of line
 	  for (unsigned int i = 0; i < lines->size(); i++) {
 		  for (unsigned int j = i + 1; j <= lines->size(); j++) {
-			  // Which point of the first line has lowest value and bigest value
-			  if ((*lines)[i][X1] < (*lines)[i][X2]) {
-			    lowest_X_first_line_pointer = &(*lines)[i][X1];
-			    bigest_X_first_line_pointer = &(*lines)[i][X2];
+			  // Which points of the first line has lowest and bigest Y value
+			  if ((*lines)[i][Y1] < (*lines)[i][Y2]) {
+			    lowest_Y_first_line_pointer = &(*lines)[i][Y1];
+			    bigest_Y_first_line_pointer = &(*lines)[i][Y2];
 			  } else {
-			    lowest_X_first_line_pointer = &(*lines)[i][X2];
-			    bigest_X_first_line_pointer = &(*lines)[i][X1];
+			    lowest_Y_first_line_pointer = &(*lines)[i][Y2];
+			    bigest_Y_first_line_pointer = &(*lines)[i][Y1];
 			  }
 
-			  // Which point of the second line has lowest value and bigest value
-			  if ((*lines)[j][X1] < (*lines)[j][X2]) {
-			    lowest_X_second_line_pointer = &(*lines)[j][X1];
-				bigest_X_second_line_pointer = &(*lines)[j][X2];
+			  // Which points of the second line has lowest and bigest Y value
+			  if ((*lines)[j][Y1] < (*lines)[j][Y2]) {
+			    lowest_Y_second_line_pointer = &(*lines)[j][Y1];
+			    bigest_Y_second_line_pointer = &(*lines)[j][Y2];
 			  } else {
-				  lowest_X_second_line_pointer = &(*lines)[j][X2];
-				  bigest_X_second_line_pointer = &(*lines)[j][X1];
+				  lowest_Y_second_line_pointer = &(*lines)[j][Y2];
+				  bigest_Y_second_line_pointer = &(*lines)[j][Y1];
 			  }
 
-			  if ((*lowest_X_first_line_pointer < *bigest_X_second_line_pointer) &&
-			      (*lowest_X_second_line_pointer < *bigest_X_first_line_pointer)) {
-			    filtred_line_pairs->push_back(std::pair<cv::Vec4i, cv::Vec4i>((*lines)[i], (*lines)[j])); // Add filtred pair of line
+			  if ((*lowest_Y_first_line_pointer < *bigest_Y_second_line_pointer) &&
+			      (*lowest_Y_second_line_pointer < *bigest_Y_first_line_pointer)) {  // One line isn't hover the other
+			    // Which points of the first line has lowest and bigest X value
+	        if ((*lines)[i][X1] < (*lines)[i][X2]) {
+	          lowest_X_first_line_pointer = &(*lines)[i][X1];
+	          bigest_X_first_line_pointer = &(*lines)[i][X2];
+	        } else {
+	          lowest_X_first_line_pointer = &(*lines)[i][X2];
+	          bigest_X_first_line_pointer = &(*lines)[i][X1];
+	        }
+
+	        // Which points of the second line has lowest and bigest X value
+	        if ((*lines)[j][X1] < (*lines)[j][X2]) {
+	          lowest_X_second_line_pointer = &(*lines)[j][X1];
+	          bigest_X_second_line_pointer = &(*lines)[j][X2];
+	        } else {
+	          lowest_X_second_line_pointer = &(*lines)[j][X2];
+	          bigest_X_second_line_pointer = &(*lines)[j][X1];
+	        }
+
+	        if (!VisibleCrossLine(*lowest_X_first_line_pointer,
+	                              *bigest_X_first_line_pointer,
+	                              *lowest_X_second_line_pointer,
+	                              *bigest_X_second_line_pointer)) {
+
+	          cv::Vec4i first_line(*lowest_X_first_line_pointer,
+	                               *lowest_Y_first_line_pointer,
+	                               *bigest_X_first_line_pointer,
+	                               *bigest_Y_first_line_pointer);
+	          cv::Vec4i second_line(*lowest_X_second_line_pointer,
+                                  *lowest_Y_second_line_pointer,
+                                  *bigest_X_second_line_pointer,
+                                  *bigest_Y_second_line_pointer);
+
+	          if (*lowest_X_first_line_pointer < *lowest_X_second_line_pointer) {
+	            filtred_line_pairs->push_back(std::pair<cv::Vec4i, cv::Vec4i>(first_line, second_line)); // Add filtred pair of line
+	          } else {
+              filtred_line_pairs->push_back(std::pair<cv::Vec4i, cv::Vec4i>(second_line, first_line)); // Add filtred pair of line
+	          }
+	        }
 			  }
 		  }
 	  }
@@ -66,4 +107,19 @@ Data FiltredVerticalLinePairsNode::Function(InputData input_data) {
   }
 
   return output_data;
+}
+
+bool FiltredVerticalLinePairsNode ::VisibleCrossLine(
+    const int &lowest_X_first_line_pointer,
+    const int &bigest_X_first_line_pointer,
+    const int &lowest_X_second_line_pointer,
+    const int &bigest_X_second_line_pointer) {
+  if (((lowest_X_first_line_pointer < lowest_X_second_line_pointer)
+      && (bigest_X_first_line_pointer > bigest_X_second_line_pointer))
+      || ((lowest_X_second_line_pointer < lowest_X_first_line_pointer)
+      && (bigest_X_second_line_pointer > bigest_X_first_line_pointer))) {
+    return true;
+  } else {
+    return false;
+  }
 }
